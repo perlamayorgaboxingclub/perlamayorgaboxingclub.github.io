6e67e7f082a2ccb8bfc8695c3c4c73602a395189
David Rubio <davidrubiobenito@gmail.com>
2020-01-16 23:54:28 +0100

    Efecto paralax en el slider, nueva navbar, nuevas fotos centradas

--- js/app.js
+++ js/app.js
@@ -59,6 +59,30 @@ function pm_setHeightAuto(column) {
 }
 
 
+
+
+//------------------------------------------------------
+// Change style of navbar on scroll
+window.onscroll = function () { 
+    myFunction();
+};
+
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    w3_close();
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top", "");
+    }
+}
+//-------------------------------------------------------
+
+
+
+
+
+
 $(document).ready(function() {
     // executes when HTML-Document is loaded and DOM is ready
 });
