85e6f3d72ce1b5199d1dab03237eb07de52b0e38
David Rubio <davidrubiobenito@gmail.com>
2020-06-21 22:21:19 +0200

    cambios en los precios, añadimos k1 infantil con los horarios, añadimos header y footer en la pagina de privacy, añadimos fotos efecto paralax entre secciones y fotos

---
+++ js/app-privacy.js
@@ -0,0 +1,46 @@
+// Change style of navbar on scroll
+window.onscroll = function () { 
+    scrollFunction();
+};
+
+function scrollFunction() {
+    var navbar = document.getElementById("myNavbar");
+    var pmBtnToUp = document.getElementById("pm-btnToUp");
+    w3_close();
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+        pmBtnToUp.style.display = "block";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top", " w3-white w3-opacity");
+        pmBtnToUp.style.display = "none";
+    }
+}
+
+
+// Toggle between showing and hiding the sidebar when clicking the menu icon
+var mySidebar = document.getElementById("mySidebar");
+
+function w3_open() {
+    if (mySidebar.style.display === 'block') {
+        mySidebar.style.display = 'none';
+    } else {
+        mySidebar.style.display = 'block';
+    }
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+    mySidebar.style.display = "none";
+}
+
+
+$(document).ready(function() {
+    // executes when HTML-Document is loaded and DOM is ready
+});
+   
+$(window).on('load', function() {
+    // executes when complete page is fully loaded, including all frames, objects and images
+});
+
+$(window).resize(function() {
+});
